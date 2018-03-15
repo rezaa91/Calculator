d375c71dc6c4bc7df14ddb4f3672512f3f0153b1
Rezaa91 <rezaa91@hotmail.co.uk>
2018-03-15 22:51:59 +0000

    Created layout component for application

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+
+
+//components
+import Screen from './component/screen.js';
+import Btn from './component/btn.js';
+
+
+class Layout extends React.Component{
+    render(){
+        return(
+            <div className="wrapper">
+                <Screen />
+            
+                <br />
+            
+                <Btn character="C" class="clear" />
+                <Btn character="/" class="btn_wrapper signs" />
+            
+                <br />
+            
+                <Btn character="1" class="btn_wrapper" />
+                <Btn character="2" class="btn_wrapper" />
+                <Btn character="3" class="btn_wrapper" />
+                <Btn character="*" class="btn_wrapper signs" />
+                
+                <br />
+                <Btn character="4" class="btn_wrapper" />
+                <Btn character="5" class="btn_wrapper" />
+                <Btn character="6" class="btn_wrapper" />
+                <Btn character="-" class="btn_wrapper signs" />
+                
+                <br />
+            
+                <Btn character="7" class="btn_wrapper" />
+                <Btn character="8" class="btn_wrapper" />
+                <Btn character="9" class="btn_wrapper" />
+                <Btn character="+" class="btn_wrapper signs" />
+            
+                <br />    
+            
+                <Btn character="+/-" class="btn_wrapper" />
+                <Btn character="0" class="btn_wrapper" />
+                <Btn character="." class="btn_wrapper" />
+                <Btn character="=" class="btn_wrapper equals" />
+            
+            </div>
+        );
+    }
+}
+
+export default Layout;
