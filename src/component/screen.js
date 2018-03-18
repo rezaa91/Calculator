56f2057e16b3012408ef0e690ce26d663967f5e1
Rezaa <rezaa91@hotmail.co.uk>
2018-03-18 18:40:22 +0000

    removed unnecessary props

--- src/component/screen.js
+++ src/component/screen.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 
 class Screen extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            display: 0
-        }
-    }
+    
     render(){
         return(
             <div id="screen">
-                <div id="sum">{this.state.sum}</div>
-                <div id="answer">{this.state.display}</div>
+                <div id="sum"></div>
+                <div id="answer">0</div>
             </div>
         );
     }
